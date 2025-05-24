dc32b89a52a9000e054e99ea996f18e3b8d51084
Deepak Bagada <deeepakbagada25@gmail.com>
2025-05-24 18:01:02 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:
    
    And change it as follows:
    
    use the path src/public/saasnextweb.png

--- src/components/sections/portfolio-section.tsx
+++ src/components/sections/portfolio-section.tsx
@@ -45,7 +45,7 @@ const portfolioItems = [
     projectUrl: "#",
     imageElement: (
       <Image
-        src={webfile} 
+        src={webfile} // Using saasnextweb.png here as well based on previous context
         alt="Targeted Lead Generation Campaign"
         fill
         sizes="(min-width: 640px) 50vw, 100vw"
@@ -61,7 +61,7 @@ const portfolioItems = [
     projectUrl: "#",
     imageElement: (
       <Image
-        src={webfile2} 
+        src={webfile2} // Using saasnextai.png here
         alt="E-commerce Store Overhaul"
         fill
         sizes="(min-width: 640px) 50vw, 100vw"
