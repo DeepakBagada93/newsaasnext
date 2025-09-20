dfa21be56f198874e8dddbea26c07e0bc1eb9723
Deepak Bagada <deeepakbagada25@gmail.com>
2025-09-20 16:50:18 +0000

    Try fixing this error: `Server Error: Error: Publishable key not valid..

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  }
 };
 
 export default nextConfig;
